59b60d7836734ff54dbedf287438a2b5a82946cc
arun1929 <84227806+arun1929@users.noreply.github.com>
2021-05-18 21:07:00 +0530

    Add files via upload

---
+++ pythagorean.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<cs50.h>
+int main(void)
+{
+	float a,b,c;
+	a = get_float("side 1 ");
+	b = get_float("side 2 ");
+	c = get_float("side 3 ");
+	if(a*a+b*b==c*c)
+	{
+		printf("Yes");		
+	}
+	else
+	{
+		printf("No");
+	}
+}
